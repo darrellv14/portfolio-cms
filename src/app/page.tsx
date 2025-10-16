eb54ee0609086d0f793de7ab119d58423ae95098
Darrell Valentino <31069810+darrellv14@users.noreply.github.com>
2025-10-16 09:54:08 +0000

    fix mobile layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,7 +95,7 @@ export default function HomePage() {
       <Separator />
 
       <section id="testimonials" className="space-y-6">
-        <div className="flex items-start justify-between gap-4">
+        <div className="flex flex-col items-start gap-4 md:flex-row md:items-center md:justify-between">
           <div className="flex-1 space-y-2">
             <h1 className="text-xl font-bold lg:text-4xl">Testimonials</h1>
             <p className="text-muted-foreground text-justify text-sm leading-relaxed font-normal tracking-wide lg:text-xl">
