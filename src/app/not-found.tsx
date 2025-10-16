eea004640a1897da057016ea6521784645a6e969
Darrell Valentino <31069810+darrellv14@users.noreply.github.com>
2025-10-16 09:00:32 +0000

    add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { Button } from "~/components/ui/button";
+import { AlertTriangle } from "lucide-react";
+
+export default function NotFound() {
+  return (
+    <div className="flex min-h-[60vh] flex-col items-center justify-center space-y-6 text-center">
+      <AlertTriangle className="h-24 w-24 text-yellow-500" />
+      <div className="space-y-2">
+        <h1 className="text-2xl font-extrabold tracking-tight lg:text-4xl">
+          404 - Page Not Found, No noo yahh!
+        </h1>
+        <p className="text-muted-foreground text-lg">
+          Oops, you are lost in the dark rainforest, let's go back!
+        </p>
+      </div>
+      <Button asChild>
+        <Link href="/">Back to Homepage!</Link>
+      </Button>
+    </div>
+  );
+}
