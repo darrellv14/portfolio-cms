9f1c4a1ce5b91fb3923b86ffd4edaffde8aece7c
github_darrellv14 <darrell.valentino14@gmail.com>
2025-10-18 19:38:31 +0700

    fix error

--- src/server/db.ts
+++ src/server/db.ts
@@ -4,6 +4,7 @@ import { env } from "~/env";
 
 const createPrismaClient = () => {
   const client = new PrismaClient({
+    datasourceUrl: env.DATABASE_URL,
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
@@ -15,5 +16,4 @@ const globalForPrisma = globalThis as unknown as {
 };
 
 export const db = globalForPrisma.prisma ?? createPrismaClient();
-
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
